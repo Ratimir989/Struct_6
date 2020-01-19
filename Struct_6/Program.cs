e446f8fde2a8b7a1245105b488bf695c1760c028
Ratimir <ra0953029364@gmail.com>
2020-01-20 01:58:57 +0200

    Добавьте файлы проекта.

---
+++ Struct_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Struct_6
+{
+    class Accauntant
+    {
+        public bool AskForBonus(Position worker, int hours)
+        {
+            if ((int)worker < hours)
+                return true;
+            return false;
+        }
+    }
+    enum Position { QualityAssurance = 40, ProjectManager = 60, HumanResources = 80, SeniorDeveloper = 100 }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant acc = new Accauntant();
+            Position senDeveloer = Position.SeniorDeveloper;
+            Console.WriteLine(acc.AskForBonus(senDeveloer, 101) ? "Senior Developer has premium" : "Senior Developer hasnt premium");
+            Console.ReadKey();
+        }
+    }
+}
